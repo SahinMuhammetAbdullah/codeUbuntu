1b4d4488909f7698b89665510a3ffaa812d562a3
SahinMuhammetAbdulah <m.sahingithub@gmail.com>
2023-11-17 17:16:41 +0300

    main

--- test.c
+++ test.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char const *argv[])
-{
-    printf("hello\n");
-    return 0;
+void drd1() {
+  fork();
+  fork();
+  printf("Hello\n");
+  return;
+}
+void drd2() {
+  if (fork() == 0) {
+    fork();
+    printf("Hello\n");
+    exit(0);
+  }
+  return;
+}
+void drd3() {
+  if (fork() == 0) {
+    fork();
+    printf("Hello\n");
+    return;
+  }
+  return;
+}
+int main() {
+  drd3();
+  printf("Hello\n");
+  exit(0);
+  return 0;
 }
